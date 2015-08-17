fa9a8b3ba316b7c8b8efd6432ca8b4948b6310a4
commander <commander@commander.(none)>
2015-08-16 22:48:13 -0400

    gg part2

--- hw0_part2.rb
+++ hw0_part2.rb
@@ -1,40 +1,20 @@
 def hello(name)
-	print "Hello, ", name, "\n"
+    print "Hello, ", name, "\n"
 end
 
 def starts_with_consonant?(s)
-	case s
-	when /^[^aeiou]/i; 	puts true
-	else;				puts false
-	end
+    case s
+    when /^[^aeiou]/i;  return true
+    else;               return false
+    end
 end
 
 def binary_multiple_of_4?(s)
-	case s
-	when /^[10]*00$/; 	puts true
-	else;				puts false
-	end
+    case s
+    when /^[10]*00$/;   return true
+    else;               return false
+    end
 end
 
-
-
-/********* LEO VERSION ********/
-
-
-def hello(name)
-	return "Hello, " + name
-end
-
-def starts_with_consonant?(s)
-	case s
-	when /^[^aeiou]/i; 	return true
-	else;			return false
-	end
-end
-
-def binary_multiple_of_4?(s)
-	case s
-	when /^[10]*00$/; 	return true
-	else;			return false
-	end
-end
+#a = "albert"
+#starts_with_consonant?(a)
