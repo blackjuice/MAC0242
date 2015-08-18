39d844005f0046bac62d6a14ddcf22c46e7c4ddb
Lucas Hong <lucas.sj.hong@gmail.com>
2015-08-18 13:32:48 -0300

    working babe

--- hw0_part2.rb
+++ hw0_part2.rb
@@ -1,7 +1,7 @@
 =begin
-	Integrantes: 
-	Leonardo Daneu Lopes	8516816
-	Lucas Sung Jun Hong	8124329
+    Integrantes: 
+    Leonardo Daneu Lopes    8516816
+    Lucas Sung Jun Hong 8124329
 =end
 
 def hello(name)
@@ -10,17 +10,14 @@ end
 
 def starts_with_consonant?(s)
     case s
-    when /^[^aeiou]/i;  return true
-    else;               return false
+    when /^[a-z&&[^aeiou]]/i;   return true
+    else;                       return false
     end
 end
 
 def binary_multiple_of_4?(s)
     case s
-    when /^[10]*00$/;   return true
+    when /^[10]*0+$/;   return true
     else;               return false
     end
 end
-
-#a = "albert"
-#starts_with_consonant?(a)
