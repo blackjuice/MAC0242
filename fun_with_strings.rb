b5c1da9ca1cc5a208d87692a082eeaab585c5059
leodaneu <leonardodaneu93@gmail.com>
2015-09-09 18:21:49 -0300

    Create fun_with_strings.rb

---
+++ fun_with_strings.rb
@@ -0,0 +1,25 @@
+module FunWithStrings
+    def palindrome?
+        str = self.downcase.gsub(/[^a-z]/,'')
+        if (str.reverse == str) 
+            return true
+        end
+        return false
+    end
+
+    def count_words
+        str = {}
+        self.downcase.gsub(/[!.,-]/, '').split.each {|word| str.store(word, str[word].to_i + 1)}
+        return str
+    end
+
+  def anagram_groups
+       return self.downcase.split.group_by {|word| word.chars.sort}.values
+  end
+end
+
+# make all the above functions available as instance methods on Strings:
+
+class String
+  include FunWithStrings
+end
