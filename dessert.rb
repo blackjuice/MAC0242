a8143db697f39c89921d6b12bed2b25266b42a48
leodaneu <leonardodaneu93@gmail.com>
2015-09-09 18:58:05 -0300

    Create dessert.rb

---
+++ dessert.rb
@@ -0,0 +1,37 @@
+class Dessert
+
+    attr_accessor :name, :calories
+
+    def initialize name, calories
+        @name, @calories = name, calories
+    end
+
+    def healthy?
+        if @calories < 200
+            return true
+        return false
+    end
+    
+    def deicious?
+        return true
+    end
+    
+end
+
+class JellyBean < Dessert
+
+    attr_accessor :flavour
+
+    def initialize flavour
+        super(flavour + " jelly bean", 5)
+        @flavour = flavour
+    end
+
+    def deicious?
+        if @flavour = "licorice"
+            return false
+        end  
+        return true
+    end 
+
+end
