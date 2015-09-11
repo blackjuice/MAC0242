3505f15c0f64b54967a301720ba1f04bcbc0a3c7
Lucas Hong <lucas.sj.hong@gmail.com>
2015-09-11 17:10:37 -0300

    updated

--- attr_accessor_with_history.rb
+++ attr_accessor_with_history.rb
@@ -1,9 +1,31 @@
+=begin
+  Integrantes: 
+  Leonardo Daneu Lopes  8516816
+  Lucas Sung Jun Hong 8124329
+=end
+
 class Class
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
+        def #{attr_name}
+            @#{attr_name}
+        end
+        def #{attr_name}= val
+            @#{attr_name}_history ||= []
+            @#{attr_name}_history << #{attr_name}
+            @#{attr_name} = val
+        end
+        def #{attr_name}_history
+            @#{attr_name}_history
+        end
+        }
+  end
+end
+
+=begin
             def #{attr_name}=(value)
                 if !defined? @#{attr_name}_history
                     @#{attr_name}_history = [@#{attr_name}]
@@ -11,7 +33,4 @@ class Class
                 @#{attr_name} = value
                 @#{attr_name}_history << value
             end
-
-    }
-  end
-end
+=end
